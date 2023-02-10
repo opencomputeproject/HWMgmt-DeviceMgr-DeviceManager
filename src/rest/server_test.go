58e70b98938a0f0ba9c894b2cafb17e37aa8c723
Emilia Haligowska <emilia.haligowska@intel.com>
2023-02-10 11:30:41 -0800

    Tests for Status.
    Signed-off-by: Emilia Haligowska <emilia.haligowska@intel.com>

--- src/rest/server_test.go
+++ src/rest/server_test.go
@@ -2,9 +2,17 @@ package rest
 
 import (
 	"devicemanager/config"
+	"github.com/kataras/iris/v12"
 )
 
 var configForTesting = config.Config{
 	UserName: "admin",
 	Password: "WjivsaGNQI5s02f525Hiq5vOg0za09okGDzBdJVsIM413TnCvTaq6QcRGuPWraNT92l6XxqPxWeq6eTKQanRnQ==",
+}
+
+func testApp() *iris.Application {
+	app := iris.New()
+	app.WrapRouter(trailingSlashRouter)
+	createRouting(app, configForTesting)
+	return app
 }
