804cc85a17607105f7b2405e21594a7c4cd65d48
Maciej Mis <maciej.mis@intel.com>
2023-03-24 13:03:55 +0100

    Updated build scripts.
    Signed-off-by: Maciej Mis <maciej.mis@intel.com>

--- build/redis/createSchema.sh
+++ build/redis/createSchema.sh
@@ -1,4 +1,4 @@
-/opt/deviceManager/redis/src/redis-cli <<HERE
+redis-cli -p 6380 <<HERE
 Set  "registry:assignedprivileges"  '{"List":["Login", "ConfigureManager", "ConfigureUsers", "ConfigureSelf", "ConfigureComponents"]}'
 Set "roles:redfishdefined"  '{"List":["Administrator", "Operator", "ReadOnly"]}'
 Set "User:admin"  '{"UserName":"admin","Password":"O01bKrP7Tzs7YoO3YvQt4pRa2J_R6HI34ZfP4MxbqNIYAVQVt2ewGXmhjvBfzMifM7bHFccXKGmdHvj3hY44Hw==","RoleId":"Administrator", "AccountTypes":["Redfish"]}'
--- build/runODIMServices.sh
+++ build/runODIMServices.sh
@@ -1,12 +1,16 @@
+#!/usr/bin/env bash
+
 svcs=("svc-account-session" "svc-aggregation" "svc-api" "svc-events" "svc-systems" "svc-task")
 port=45101
 for svc in ${svcs[@]}; do
   if [ $svc == 'svc-api' ]
   then
-     ${svc}/${svc} --registry_address=127.0.0.1:2379 --server_address=127.0.0.1 &
+     ${svc} --registry_address=etcd:2379 --server_address=127.0.0.1 >> /var/log/deviceManager/${svc}.log 2>&1 &
   else
-  echo "${svc}/${svc} --registry_address=127.0.0.1:2379 --server_address=127.0.0.1:${port}"
-  ${svc}/${svc} --registry_address=127.0.0.1:2379 --server_address=127.0.0.1:${port} &
+  echo "${svc} --registry_address=etcd:2379 --server_address=127.0.0.1:${port}"
+  ${svc} --registry_address=etcd:2379 --server_address=127.0.0.1:${port} >> /var/log/deviceManager/${svc}.log 2>&1 &
   ((port=port+1))
   fi
 done
+
+wait
