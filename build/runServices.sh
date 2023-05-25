65400f88605e4149569fa9ad76af85310a36398e
Emilia Haligowska <emilia.haligowska@intel.com>
2023-05-25 15:53:42 -0700

    Added svc-update and svc-managers to scripts building and running services.
    Signed-off-by: Emilia Haligowska <emilia.haligowska@intel.com>

--- build/runServices.sh
+++ build/runServices.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-svcs=("svc-account-session" "svc-aggregation" "svc-api" "svc-events" "svc-systems" "svc-task")
+svcs=("svc-account-session" "svc-aggregation" "svc-api" "svc-events" "svc-systems" "svc-task" "svc-update" "svc-managers")
 port=45101
 for svc in ${svcs[@]}; do
   if [ $svc == 'svc-api' ]
@@ -10,6 +10,10 @@ for svc in ${svcs[@]}; do
   echo "${svc} --registry_address=etcd:2379 --server_address=device-manager:${port}"
   ${svc} --registry_address=etcd:2379 --server_address=device-manager:${port} >> /var/log/deviceManager/${svc}.log 2>&1 &
   ((port=port+1))
+  if [ $svc == 'svc-task' ]
+  then
+    ((port=port+1))
+  fi
   fi
 done
 
