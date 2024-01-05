89aa28533a6bb9fe9ba8f63c4852525f232197d2
Maciej Mis <maciej.mis@intel.com>
2024-01-05 16:26:32 +0100

    Integration of TelemetryService in DeviceManager.
    Signed-off-by: Maciej Mis <maciej.mis@intel.com>

--- build/buildServices.sh
+++ build/buildServices.sh
@@ -1,4 +1,4 @@
-services=("svc-account-session" "svc-aggregation" "svc-api" "svc-events" "svc-task" "svc-systems" "svc-update" "svc-managers")
+services=("svc-account-session" "svc-aggregation" "svc-api" "svc-events" "svc-task" "svc-systems" "svc-update" "svc-managers" "svc-telemetry")
 for service in ${services[@]}; do
   cd "$service"
   $GOROOT/bin/go build -o ../apps
